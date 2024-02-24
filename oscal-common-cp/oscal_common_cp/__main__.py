a000bb08b740133ca85c14905213e26b25936f7c
Robert Sherwood <robert.sherwood@credentive.com>
2024-02-24 12:22:10 +0000

    parsing contents to produce catalog

--- oscal-common-cp/oscal_common_cp/__main__.py
+++ oscal-common-cp/oscal_common_cp/__main__.py
@@ -5,6 +5,7 @@ import re
 import uuid
 from pathlib import Path
 import argparse
+from html.parser import HTMLParser
 
 # This program relies heavily on the specific format of the tokenized CP documents.
 
@@ -37,40 +38,137 @@ def common_policy_to_catalog(common_policy: list[str]) -> document.Document:
     # The first list is always the introduction/metadata
     metadata = parse_metadata(sections[0])
 
+    # We keep a stack to represent the current place in the TOC
+    parent_stack: list[catalog.Group] = []
+
+    section_groups: list[catalog.Group] = []
+
+    # Step through the sections and generate the appropriate OSCAL objects.
+    for section in sections[1:]:
+        # The first line has the section title, and because it is MD,
+        # the number of hashes indicate the depth in the TOC
+        header_hashes = re.match(r"#+", section[0])
+        if header_hashes is not None:
+            section_depth = len(header_hashes.group(0))
+            current_group = section_to_group(section)
+            if current_group.title.strip() == "":
+                # Sometimes we get junk headers in the Markdown
+                continue
+            if section_depth == 1:
+                if len(parent_stack) == 0:
+                    # e.g. this is the first run through the loop
+                    parent_stack = [current_group]
+                else:
+                    parent_stack[0] = current_group
+                # Add the top-level group to the final list of groups
+                section_groups.append(parent_stack[0])
+            else:
+                # Careful! If we jump more than one level at a time, bad things could happen
+                if section_depth > len(parent_stack):
+                    parent_stack.append(current_group)
+                    # Add current group to the section above
+                    add_subsection_to_parent(
+                        parent_stack[section_depth - 2], current_group
+                    )
+                elif section_depth == len(parent_stack):
+                    # Replace the previous TOC leaf node with current
+                    parent_stack[section_depth - 1] = current_group
+                    add_subsection_to_parent(
+                        parent_stack[section_depth - 2], current_group
+                    )
+                elif section_depth < len(parent_stack):
+                    # Trim the stack
+                    parent_stack = parent_stack[:section_depth]
+                    # Replace TOC leaf node with current
+                    parent_stack[section_depth - 1] = current_group
+                    add_subsection_to_parent(
+                        parent_stack[section_depth - 2], current_group
+                    )
+        else:
+            raise Exception("Section does not have a title")
+
     # Ignoring backmatter for now
     backmatter = parse_backmatter([])
 
     common_catalog = catalog.Catalog(
         uuid=uuid.uuid4(),
         metadata=metadata,
+        groups=section_groups,
         back_matter=backmatter,
     )
 
     return document.Document(catalog=common_catalog)
 
 
+def add_subsection_to_parent(
+    parent: catalog.Group, child: catalog.Group
+) -> catalog.Group:
+    if parent.groups is None:
+        parent.groups = [child]
+    else:
+        parent.groups.append(child)
+
+    return parent
+
+
 def section_to_group(section_contents: list[str]) -> catalog.Group:
-    pass
+    # Strip off the leading hashes and the trailing space
+    group_title = re.sub("#+ ", "", section_contents[0])
+
+    section_group = catalog.Group(
+        id=group_title.lower().replace(
+            " ", "-"
+        ),  # "Section Title" becomes "section-title"
+        title=group_title,
+    )
+
+    if len(section_contents) > 1:
+        # The section contains requirements, and must have a control
+        section_control_list: list[catalog.Control] | None = [
+            section_to_control(title=group_title, section_contents=section_contents)
+        ]
+    else:
+        section_control_list = None
+
+    section_group.controls = section_control_list
+
+    return section_group
 
 
-def section_to_control(section_contents: list[str]) -> catalog.Control:
-    pass
+def section_to_control(title: str, section_contents: list[str]) -> catalog.Control:
+    control = catalog.Control(
+        id=title.lower().replace(" ", "-"),  # "Section Title" becomes "section-title",
+        title=title,
+        parts=[],
+    )
 
+    parts: list[catalog.BasePart] = []
+    for part_num, part in enumerate(section_contents[1:]):
+        parts.append(
+            catalog.StatementPart(
+                id=f"{id}-{part_num}",
+                name="statement",
+                prose=part,
+            )
+        )
 
-def statement_to_part(statement: str) -> catalog.BasePart:
-    pass
+    control.parts = parts
+
+    return control
 
 
 def parse_metadata(introduction: list[str]) -> common.Metadata:
     version = ""
     published = None
-    for line in introduction:
+    for line_number, line in enumerate(introduction):
         if line == "":
             # Blank line, ignore and move on
             continue
-        elif "<table>" in line:
+        elif "</table>" in line:
             # Revision history is maintained in a table - parse it
-            # TODO: Parse Revision History
+            # Function works backwards from the END of a table, hence </table>
+            revision_table = parse_html_table(introduction, line_number)
+            # TODO: Create Revision table
             continue
         elif re.match(r"\[\d.*", line) is not None:
             # This could be a TOC entry - we can derive the section number from this.
@@ -104,7 +202,58 @@ def parse_metadata(introduction: list[str]) -> common.Metadata:
 
 
 def parse_backmatter(contents: list[str]) -> common.BackMatter:
-    pass
+    return common.BackMatter(resources=None)
+
+
+def parse_html_table(contents: list[str], table_end_line: int) -> list[list[str]]:
+    current_line = table_end_line
+    while contents[current_line] != "<table>":
+        current_line -= 1
+
+    table_list = contents[current_line : table_end_line + 1]
+
+    # It's weird to define a class inside a function, but this is how HTMLParser works.
+    class TableParser(HTMLParser):
+        parsed_table: list[list[str]] = []
+        current_row: list[str] = []
+        current_cell: str
+        in_row: bool = False
+        in_cell: bool = False
+
+        def handle_starttag(
+            self, tag: str, attrs: list[tuple[str, str | None]]
+        ) -> None:
+            if tag == "tr":
+                # We're starting a new row
+                self.current_row = []
+                self.in_row = True
+            if tag == "td":
+                self.current_cell = ""
+                self.in_cell = True
+
+        def handle_endtag(self, tag: str) -> None:
+            if tag == "tr":
+                self.parsed_table.append(self.current_row)
+                self.in_row = False
+            if tag == "td":
+                self.current_row.append(self.current_cell)
+                self.in_cell = False
+            else:
+                # There are some style tags in the rows - we want a space between the contents
+                if self.in_row and self.in_cell:
+                    # Add a space to the cell we're processing now.
+                    self.current_cell = self.current_cell + " "
+
+        def handle_data(self, data: str) -> None:
+            if self.in_row and self.in_cell:
+                self.current_cell = self.current_cell + data
+
+        def return_results(self) -> list[list[str]]:
+            return self.parsed_table
+
+    table_parser = TableParser()
+    table_parser.feed("".join(table_list))
+    return table_parser.return_results()
 
 
 if __name__ == "__main__":
